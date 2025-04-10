7c1d39d20eeef83e0569aa25c572f4196d3a2f85
rajshivam-2003 <shivam.raj@revomo.com>
2025-04-10 10:48:36 +0530

    first commit

---
+++ project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold underline text-center mt-10'>
+      Hello world!
+    </h1>
+    <h2 className='text-2xl font-bold text-center mt-10'>
+      This is a fullstack assignment  
+      </h2>
+    </>
+  )
+}
+
+export default App
